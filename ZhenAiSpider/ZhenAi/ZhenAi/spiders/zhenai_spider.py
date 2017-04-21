dd5255902106cd1402aaf24245929f3297e7202d
huangchuchuan <huangchuchuan@126.com>
2017-04-20 21:29:41 -0400

    change fixed pages' number to use url_generator to generate start_urls; fix bugs to extra zhima_info

--- ZhenAiSpider/ZhenAi/ZhenAi/spiders/zhenai_spider.py
+++ ZhenAiSpider/ZhenAi/ZhenAi/spiders/zhenai_spider.py
@@ -12,14 +12,71 @@ from scrapy.utils.response import get_base_url
 class ZhenaiSpider(scrapy.Spider):
     name = 'zhenai_spider'
     # generate all base urls
-    base_url = 'http://search.zhenai.com/v2/search/getPinterestData.do?' \
-               'sex=%d&agebegin=18&ageend=-1&workcityprovince=-1&workcitycity=-1' \
-               '&info=&h1=-1&h2=-1&salaryBegin=-1&salaryEnd=-1&occupation=-1&h=-1' \
-               '&c=-1&workcityprovince1=-1&workcitycity1=-1&constellation=-1&animals=-1' \
-               '&stock=-1&belief=-1&lvBegin=-1&lvEnd=-1&condition=66&orderby=hpf&hotIndex=&online=' \
-               '&currentpage=%d&topSearch=false'
-    max_pages = 150
-    start_urls = [base_url % (i, j) for i in range(0, 2) for j in range(1, max_pages + 1)]
+    base_url = 'http://search.zhenai.com/v2/search/getPinterestData.do?sex={}&agebegin={}&ageend={}&workcityprovince={}' \
+               '&workcitycity={}&education={}' \
+               '&occupation={}&info=&marriage={}&h1={}&h2={}&salaryBegin={}&salaryEnd={}' \
+               '&h={}&c={}&workcityprovince1={}&workcitycity1={}&constellation={}&animals={}&stock={}&belief={}' \
+               '&lvBegin={}&lvEnd={}&condition=66&orderby=hpf&hotIndex=&online={}&currentpage={}&topSearch=false'
+    sex = [0, 1]
+    agebegin = range(18, 100)
+    agebegin.append(-1)
+    agebegin.reverse()
+    ageend = range(18, 100)
+    ageend.append(-1)
+    ageend.reverse()
+    workcityprovince = [-1]  # TODO
+    workcitycity = [-1]  # TODO
+    education = range(2, 8)
+    education.append(-1)
+    education.reverse()
+    occupation = range(100, 2900, 100)
+    occupation.append(-1)
+    occupation.reverse()
+    marriage = [-1, 1, 3, 4]  # weihun, liyi, sang'ou
+    h1 = range(129, 212)
+    h1.append(-1)
+    h1.reverse()
+    h2 = range(129, 212)
+    h2.append(-1)
+    h2.reverse()
+    salaryBegin = range(103, 109)  # yue shou ru
+    salaryBegin.append(-1)
+    salaryBegin.reverse()
+    salaryEnd = range(103, 109)
+    salaryEnd.append(-1)
+    salaryEnd.reverse()
+    h = range(1, 6)  # house
+    h.append(-1)
+    h.reverse()
+    c = range(1, 6)  # children
+    c.append(-1)
+    c.reverse()
+    workcityprovince1 = [-1]  # TODO
+    workcitycity1 = [-1]  # TODO
+    constellation = range(1, 13)  # xing zuo
+    constellation.append(-1)
+    constellation.reverse()
+    animals = range(1, 13)  # sheng xiao
+    animals.append(-1)
+    animals.reverse()
+    stock = range(1, 58)  # min zu
+    stock.append(-1)
+    stock.reverse()
+    belief = range(1, 14)  # not sure
+    belief.append(-1)
+    belief.reverse()
+    lvBegin = range(1, 8)
+    lvBegin.append(-1)
+    lvBegin.reverse()
+    lvEnd = range(1, 8)
+    lvEnd.append(-1)
+    lvEnd.reverse()
+    online = [-1, 1]
+    currentpage = range(1, 101)
+    start_urls = url_generator(base_url, sex, agebegin, ageend, workcityprovince, workcitycity,
+                               education, occupation, marriage, h1, h2, salaryBegin, salaryEnd, h, c,
+                               workcityprovince1, workcitycity1, constellation, animals, stock, belief, lvBegin, lvEnd,
+                               online, currentpage)
 
     def parse(self, response):
         data = {}
@@ -50,8 +107,10 @@ class ZhenaiSpider(scrapy.Spider):
         if len(honesty_charm) == 2:
             honesty = honesty_charm[0]
             charm = honesty_charm[1]
-        zhima_info = html_selector.xpath('//p[@class="brief-name lh32 blue"]/a[6]/text()').extract_first().replace(
-            u'\u5206', '')
+        zhima_info = html_selector.xpath(
+            '//p[@class="brief-name lh32 blue"]//a[@class="flag-credit credit-js"]/text()').extract_first()
+        if zhima_info:
+            zhima_info = zhima_info.replace(u'\u5206', '')
         if not zhima_info.isdigit():
             zhima_info = '--'
 
